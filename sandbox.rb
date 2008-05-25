6bab05418e1bb54fe7303eeca8e00c8acefe5623
jp_tix <kreatix@gmail.com>
2008-05-25 20:14:33 +0200

    Update RCB templates.

--- sandbox.rb
+++ sandbox.rb
@@ -1,7 +1,7 @@
 require "lib/ri_outputter"
 
 ri = RiOutputter::Lookup.new(:template_folder => File.dirname(__FILE__) + "/lib/ri_outputter/templates/ruby_class_browser")
-puts ri.html_for("String")
-# puts ri.html_for('Set#collect')
+# puts ri.html_for("String")
+# puts ri.html_for('Array#collect')
 # puts ri.html_for('String#sp')
-# puts ri.html_for('St')
+puts ri.html_for('St')
